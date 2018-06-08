a7a2170525e289b8ca42adb89b906245e71c2fa9
IAmArr0w <bkmzilia@gmail.com>
2018-06-08 11:02:57 +0300

    DLETE USER

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -4,23 +4,120 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Book;
+use App\Comment;
+use App\User;
+use App\Order;
+use Intervention\Image\Facades\Image as ImageInt;
 
 class BooksController extends Controller
 {
-    public function getBooksList() {
-    	$books = Book::all();
-    	return view('main', ['books'=>$books]);
+	public function getMainPage() {
+		return view('main');
+	}
+
+	public function admin() {
+		return view('admin', ['users'=>User::all()]);
+	}
+
+    public function getBooksListArt() {
+    	$books = Book::where('type', 'art')->get();
+    	return view('art', ['books'=>$books]);
     }
 
+    public function getBooksItem($id) {
+    	$comments = Comment::where('book_id', $id)->get();
+    	$commentators = array();
+    	foreach ($comments as $comment) {
+    		array_push($commentators, User::find($comment->user_id));
+    	}
+    	return view('book', ['book'=>Book::find($id), 'comments'=>$comments, 'commentators'=>$commentators]);
+    }
+
+    public function editItem($id) {
+    	return view('edit_item', ['book'=>Book::find($id)]);
+    }
+
+    public function editBook(Request $request) {
+    	$book = Book::find($request['id']);
+
+   		if($request->file('picrel')) {
+   			$path = public_path()."\upload\\";
+			$file = $request->file('picrel');
+			$filename = str_random(20) .'.' . $file->getClientOriginalExtension() ?: 'png';
+			$img = ImageInt::make($file);
+			$img->resize(200,200)->save($path . $filename);
+			$book->update(array(
+	       			'title' => $request['title'],
+	    			'description' => $request['description'],
+	    			'type' => $request['select_type'],
+	    			'kind' => $request['select_kind'],
+	    			'author' => $request['author'],
+	    			'price' => $request['price'],
+	    			'instock' => $request['instock'],
+	    			'picrel' => "$filename",
+	    		));
+		}
+    	else {
+    		$book->update(array(
+	       			'title' => $request['title'],
+	    			'description' => $request['description'],
+	    			'type' => $request['select_type'],
+	    			'kind' => $request['select_kind'],
+	    			'author' => $request['author'],
+	    			'price' => $request['price'],
+	    			'instock' => $request['instock'],
+	    		));
+    	}
+    	return redirect()->route($request['select_type']); 
+    }
+ 
     public function addBook(Request $request) {
     	$book = new Book;
+
     	if($request->method()=='POST') {
+    		$path = public_path()."\upload\\";
+    		$file = $request->file('picrel');
+    		$filename = str_random(20) .'.' . $file->getClientOriginalExtension() ?: 'png';
+    		$img = ImageInt::make($file);
+    		$img->resize(200,200)->save($path . $filename);
+
     		$book->fill(array(
-    			'title' => $request['title'],
+       			'title' => $request['title'],
     			'description' => $request['description'],
+    			'type' => $request['select_type'],
+    			'kind' => $request['select_kind'],
+    			'author' => $request['author'],
+    			'price' => $request['price'],
+    			'instock' => $request['instock'],
+    			'picrel' => "$filename",
     		));
     		$book->save();
     	}
-    	return view('add_success');
+    	return redirect()->route('main');
+    }
+
+    public function addComment(Request $request) {
+    	$comment = new Comment;
+    	if($request->method()=='POST') {
+    		$comment->fill(array(
+    			'book_id' => $request['book'],
+    			'user_id' => $request['user'],
+    			'comment' => $request['new_comment'],
+    		));
+    		$comment->save();
+    	}
+    	return redirect()->route('item', $request['book']);
+    }
+
+    public function orderBook(Request $request) {
+    	$order = new Order;
+    	if($request->method()=='POST') {
+    		$order->fill(array(
+    			'book_id' => $request['b_id'],
+    			'user_id' => $request['us_id'],
+    		));
+    		$order->save();
+    	}
+    	return redirect()->route('main');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'BooksController@getMainPage')->name('main');
 
-Route::get('/books', 'BooksController@getBooksList')->name('books');
+Route::get('art', 'BooksController@getBooksListArt')->name('art');
 
-Route::post('add_book', 'BooksController@addBook')->name('add_book');
+Route::get('scientific', 'BooksController@getMainPage')->name('scientific');
+
+Route::get('book/{id}', 'BooksController@getBooksItem')->name('item');
+
+Route::get('admin', 'BooksController@admin')->middleware('admin')->name('admin');
+
+Route::get('edit/{id}', 'BooksController@editItem')->middleware('admin')->name('edit');
+
+Route::get('profile/{id}', 'HomeController@profilePage')->name('profile');
+
+Route::post('add_book', 'BooksController@addBook')->middleware('permission')->name('add_book');
+
+Route::post('add_comment', 'BooksController@addComment')->name('add_comment');
+
+Route::post('edit_book', 'BooksController@editBook')->middleware('admin')->name('edit_book');
+
+Route::post('order', 'BooksController@orderBook')->name('order');
+
+Route::get('adminn', 'HomeController@deleteUser')->name('delete_user');
